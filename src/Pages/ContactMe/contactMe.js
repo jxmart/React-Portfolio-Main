d02429420572d8e96cbe795524740984a3719315
James Martin <jamesmartin@Jamess-MBP.lan>
2023-06-18 01:21:11 -0400

    added on to contact page

--- src/Pages/ContactMe/contactMe.js
+++ src/Pages/ContactMe/contactMe.js
@@ -5,30 +5,30 @@ const ContactMe = () => {
         e.preventDefault();
         const form = e.target;
         if (form.checkValidity()) {
-          // Valid form submission
-          console.log('Form is valid');
-          // Add your logic for handling the form submission here
+            // Valid form submission
+            console.log('Form is valid');
+            // Add your logic for handling the form submission here
         } else {
-          // Invalid form fields
-          form.classList.add('was-validated');
+            // Invalid form fields
+            form.classList.add('was-validated');
         }
-      };
-      
+    };
+
     return (
         <div className="content d-flex justify-content-center align-items-center">
             <section className='contact'>
-                <form className='contact-form'>
+                <form className='contact-form' onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control form-control-lg" id="name" placeholder="Enter your name" />
+                        <input type="text" className="form-control form-control-lg text-center" id="name" placeholder="Enter your name" required />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control form-control-lg" id="email" placeholder="Enter your email address" />
+                        <input type="email" className="form-control form-control-lg text-center" id="email" placeholder="Enter your email address" required/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="message" className="form-label">Message</label>
-                        <textarea className="form-control form-control-lg" id="message" rows="5" placeholder="Enter your message"></textarea>
+                        <textarea className="form-control form-control-lg text-center" id="message" rows="5" placeholder="Enter your message" required></textarea>
                     </div>
                     <div className='d-grid'>
                         <button type="submit" className="btn btn-primary">Submit</button>
--- src/Pages/Portfolio/portfolio.js
+++ src/Pages/Portfolio/portfolio.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import projects from '../../Projects/projects';
+
 
 const Portfolio = () => {
+    const renderProjects = () => {
+        return projects.map((project, index) => (
+            <div key={index}>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <img src={project.image} alt={project.title} />
+                <div>
+                    <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+                        View Deployed App
+                    </a>
+                    <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                        View GitHub Repository
+                    </a>
+                </div>
+            </div>
+        ));
+    };
+
     return (
         <section className='portfolio'>
-        <h3>Portfolio</h3>
+            <h3>Portfolio</h3>
         </section>
     )
 }
